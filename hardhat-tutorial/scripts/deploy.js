2e9aea73811ef368b0277946c4b4456136ecbc1d
aakrut <aakrutdabhi@gmail.com>
2022-08-30 16:14:07 +0530

    IPFS

---
+++ hardhat-tutorial/scripts/deploy.js
@@ -0,0 +1,20 @@
+const { ethers } = require("hardhat");
+
+async function main() {
+  const metadataURL = "ipfs://QmQBHarz2WFczTjz5GnhjHrbUPDnB48W5BM2v2h6HbE1rZ";
+
+  const lw3PunksContract = await ethers.getContractFactory("LW3Punks");
+
+  const deployedLW3PunksContract = await lw3PunksContract.deploy(metadataURL);
+
+  await deployedLW3PunksContract.deployed();
+
+  console.log(`LW3Punks Contract Address: ${deployedLW3PunksContract.address}`);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
